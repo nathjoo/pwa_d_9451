7d9380e9be1589a82f47f60b8a17508a76f08a8b
jovian <njovian06@gmail.com>
2019-11-20 20:12:26 +0700

    my first commit

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,17 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <HelloKedua/>
   </div>
 </template>
+
+<script>
+import HelloKedua from '@/components/HelloKedua.vue'
+
+export default {
+  name: 'kedua',
+  components: {
+    HelloKedua
+  }
+}
+</script>
